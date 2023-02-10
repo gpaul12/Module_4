b3718c8323760d709f0b722f927b6cb070582eb2
Gerry Paul <gpaul12@nc.rr.com>
2023-02-09 19:33:33 -0500

    next commit

--- 12-Stu_Event-Listener/Unsolved/assets/js/script.js
+++ 12-Stu_Event-Listener/Unsolved/assets/js/script.js
@@ -6,7 +6,16 @@ var countEl = document.querySelector("#count");
 function setCounterText() {
   countEl.textContent = count;
 }
-
 // TODO: Add event listener to increment button
+incrementEl.addEventListener("click", function() {
+  count++;
+  setCounterText();
+})
 
 // TODO: Add event listener to decrement button 
+decrementEl.addEventListener("click", function() {
+  count--;
+  setCounterText();
+})
+
+setCounterText();
