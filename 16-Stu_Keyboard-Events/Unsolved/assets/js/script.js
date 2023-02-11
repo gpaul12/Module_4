999a95dc7cb204d7cc130e33128b1a9a7d8cff92
Gerry Paul <gpaul12@nc.rr.com>
2023-02-11 12:03:13 -0500

    next commit

--- 16-Stu_Keyboard-Events/Unsolved/assets/js/script.js
+++ 16-Stu_Keyboard-Events/Unsolved/assets/js/script.js
@@ -1,5 +1,9 @@
 function keydownAction(event) {
   // TODO: Complete keydown function
+  document.querySelector('#key').innerHTML = event.key;
+  document.querySelector('#code').innerHTML = event.code;
+  document.querySelector("#status").innerHTML = "KEYDOWN Event";
+
 }
 
 function keyupAction() {
@@ -8,4 +12,4 @@ function keyupAction() {
 
 document.addEventListener("keyup", keyupAction);
 // TODO: Add Event Listener for "keydown" event
-
+document.addEventListener("keydown", keydownAction)
