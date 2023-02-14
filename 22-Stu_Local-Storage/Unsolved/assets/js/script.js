451da142bd6d01893c28257344152c397027e848
Gerry Paul <gpaul12@nc.rr.com>
2023-02-13 19:56:33 -0500

    commit 22

--- 22-Stu_Local-Storage/Unsolved/assets/js/script.js
+++ 22-Stu_Local-Storage/Unsolved/assets/js/script.js
@@ -14,13 +14,18 @@ function displayMessage(type, message) {
 
 function renderLastRegistered() {
   // TODO: Retrieve the last email and password and render it to the page
+  var email = localStorage.getItem("emailStored");
+  var password = localStorage.getItem("passwordStored");
+
+  userEmailSpan.textContent = email;
+  userPasswordSpan.textContent = password;
 }
 
 signUpButton.addEventListener("click", function(event) {
   event.preventDefault();
 
-  var email = document.querySelector("#email").value;
-  var password = document.querySelector("#password").value;
+  var email = emailInput.value;
+  var password = passwordInput.value;
 
   if (email === "") {
     displayMessage("error", "Email cannot be blank");
@@ -28,7 +33,10 @@ signUpButton.addEventListener("click", function(event) {
     displayMessage("error", "Password cannot be blank");
   } else {
     displayMessage("success", "Registered successfully");
-
+    
   // TODO: Save email and password to localStorage and render the last registered user
+  localStorage.setItem("emailStored", email);
+  localStorage.setItem("passwordStored", password);
+  renderLastRegistered();
   }
 });
