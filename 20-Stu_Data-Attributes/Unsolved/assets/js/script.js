adfce615a76e583c68b38e217ec9ac243dbc4d7d
Gerry Paul <gpaul12@nc.rr.com>
2023-02-13 19:02:31 -0500

    next commit

--- 20-Stu_Data-Attributes/Unsolved/assets/js/script.js
+++ 20-Stu_Data-Attributes/Unsolved/assets/js/script.js
@@ -3,5 +3,15 @@ var container = document.querySelector(".container");
 container.addEventListener("click", function(event) {
   var element = event.target;
 
-  // TODO: Complete function
+  if (element.matches(".box")) {
+    var state = element.getAttribute("data-state");
+
+    if (state === "hidden") {
+      element.textContent = element.getAttribute("data-number");
+      element.setAttribute("data-state", "visible")
+    } else {
+      element.textContent= "";
+      element.setAttribute("data-state", "hidden")
+    }
+  }
 });
